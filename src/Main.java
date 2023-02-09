22ad473a6bf3517146917aaae3e88554b752a85f
Mahe0055 <mahe00555@stud.kea.dk>
2023-02-09 13:57:14 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        //Creating new instances of the classes
+    RapSong normalRap = new RapSong();
+    OldSchoolRap oldRap = new OldSchoolRap();
+    NewSchoolRap newRap = new NewSchoolRap();
+    //Calling the methods from the classes
+    normalRap.play();
+    oldRap.play();
+    newRap.play();
+    }
+}
---
+++ src/OldSchoolRap.java
@@ -0,0 +1,6 @@
+public class OldSchoolRap extends RapSong {
+    @Override
+    public void play() {
+        System.out.println("Playing old school rap");
+    }
+}
